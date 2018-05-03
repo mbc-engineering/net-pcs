e69f79f723f8af2b18f27e29108960d3a3183f33
Stefan Gmeiner <stefan.gmeiner@mbc-engineering.ch>
2018-05-03 17:07:05 +0200

    Command Event.

---
+++ NET/MbcAdcCommand/PlcCommandEventArgs.cs
@@ -0,0 +1,32 @@
+﻿namespace MbcAdcCommand
+{
+    /// <summary>
+    /// Provides information of state change events of a <see cref="PlcCommand"/>.
+    /// </summary>
+    /// <seealso cref="PlcCommand"/>
+    public class PlcCommandEventArgs
+    {
+        /// <summary>
+        /// Gets the progress value from 0 to 100 percent.
+        /// </summary>
+        public int Progress { get; }
+
+        /// <summary>
+        /// Gets the command specific sub task identifier.
+        /// </summary>
+        public int SubTask { get; }
+
+        public bool IsFinished { get; }
+
+        public bool IsCancelled { get; }
+
+        internal PlcCommandEventArgs(int progress, int subTask, bool finished, bool cancelled)
+        {
+            Progress = progress;
+            SubTask = subTask;
+            IsFinished = finished;
+            IsCancelled = cancelled;
+        }
+
+    }
+}
