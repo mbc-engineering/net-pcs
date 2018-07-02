25dfb80967fbc87a9d1ff90d573b044601208dbd
Michael Helfenstein <michael.helfenstein@mbc-engineering.ch>
2018-07-02 16:47:46 +0200

    nuspec for TwinCatAds dll

---
+++ NET/nuget-publish.ps1
@@ -0,0 +1,39 @@
+﻿Param()
+
+<#
+Script to help update the PCS Libraries on the Private Nuget stream https://mbeec.pkgs.visualstudio.com/_packaging/public/nuget/v3/index.json
+
+Require dotnet CLI tools >= 2 or >= VS 2017 15.3
+
+With the following command add credentials to a nuget.config (used the downloaded nuget version with credentials from vsts):
+.\Nuget\NuGet.exe sources add -name mbcpublic -source https://mbeec.pkgs.visualstudio.com/_packaging/public/nuget/v3/index.json -username vsts-pat -password MyVstsPat -configfile .\NuGet.Config
+#>
+
+function Main{
+    Write-Host("require minimal 2.0.0 tooling / VS 2017 15.3+")
+
+    dotnet clean -c Release
+
+    # Build libraries
+    dotnet pack -c Release --include-source --include-symbols .\Mbc.Pcs.Net\Mbc.Pcs.Net.csproj
+    dotnet pack -c Release --include-source --include-symbols .\Mbc.Pcs.Net.Test.Util\Mbc.Pcs.Net.Test.Util.csproj
+    # dotnet nuget push --source mbcpublic --api-key VSTS .\Mbc.Pcs.Net\Mbc.Pcs.Net.csproj
+
+    # Push the Packages
+}
+
+
+function CreateTwinCatAds{
+    Write-Host("require minimal 2.0.0 tooling / VS 2017 15.3+")
+
+    dotnet clean -c Release
+
+    # See nuspec refrence: https://docs.microsoft.com/en-us/nuget/reference/nuspec
+	# Install nuget.exe with: choco install nuget.commandline or download from vsts server with credentials provider
+    # Build TwinCat Library
+    NuGet.exe pack .\Mbc.Pcs.Net.TwinCat.Ads.nuspec -Version 4.2.163.1 
+    dotnet nuget push --source mbcpublic --api-key VSTS .\Mbc.Pcs.Net.TwinCat.Ads.4.2.163.1.nupkg 
+}
+
+# Execute the program
+CreateTwinCatAds
