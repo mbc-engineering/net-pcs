40f60c720e9dbaeb3b945da1cea8c6f392e0ffdf
Michael Helfenstein <michael.helfenstein@mbc-engineering.ch>
2019-01-16 09:04:12 +0100

    fix name of ressource becouse it could not be found

--- NET/Mbc.Pcs.Net/Resources.Designer.cs
+++ NET/Mbc.Pcs.Net/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Mbc.Pcs.Net {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PlcService.Ads.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mbc.Pcs.Net.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
