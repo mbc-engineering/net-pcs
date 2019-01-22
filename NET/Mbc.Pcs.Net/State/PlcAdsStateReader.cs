9cecd246c890188b15a02542fa467d8548c250bb
Stefan Gmeiner <stefan.gmeiner@mbc-engineering.ch>
2019-01-22 13:27:21 +0100

    ADS-Events werden über Queue verteilt, damit der Notification-Thread nicht blockiert wird.

--- NET/Mbc.Pcs.Net/State/PlcAdsStateReader.cs
+++ NET/Mbc.Pcs.Net/State/PlcAdsStateReader.cs
@@ -5,6 +5,7 @@
 
 using EnsureThat;
 using Mbc.Ads.Mapper;
+using Mbc.AsyncUtils;
 using Mbc.Pcs.Net.Connection;
 using NLog;
 using System;
@@ -19,9 +20,12 @@ namespace Mbc.Pcs.Net.State
 
         private readonly IPlcAdsConnectionService _adsConnectionService;
         private readonly PlcAdsStateReaderConfig<TStatus> _config;
+        private AsyncSerializedTaskExecutor _notificationExecutor;
         private IAdsSymbolInfo _adsSymbolInfo;
         private int _statusNotificationHandle;
         private AdsMapper<TStatus> _adsMapper;
+        private bool _queueOverflowLogging;
+        private int _maxOverflow;
 
         public event EventHandler<PlcStateChangedEventArgs<TStatus>> StateChanged;
 
@@ -49,7 +53,14 @@ namespace Mbc.Pcs.Net.State
                 StopSampling();
             }
 
+            _notificationExecutor = new AsyncSerializedTaskExecutor();
+            _notificationExecutor.UnhandledException = (s, e) =>
+            {
+                _logger.Error(e, "Error in notification event handler.");
+            };
+
             _adsConnectionService.Connection.AdsNotification += OnAdsNotification;
+            _adsConnectionService.Connection.AdsNotificationError += OnAdsNotificationError;
 
             ReadAdsSymbolInfo(_adsConnectionService.Connection);
 
@@ -75,7 +86,15 @@ namespace Mbc.Pcs.Net.State
                 _adsConnectionService.Connection.DeleteDeviceNotification(_statusNotificationHandle);
             }
 
+            _adsConnectionService.Connection.AdsNotificationError -= OnAdsNotificationError;
             _adsConnectionService.Connection.AdsNotification -= OnAdsNotification;
+
+            if (_notificationExecutor != null)
+            {
+                _notificationExecutor.WaitForExecution();
+                _notificationExecutor.Dispose();
+                _notificationExecutor = null;
+            }
 
             SamplingActive = false;
 
@@ -87,17 +106,26 @@ namespace Mbc.Pcs.Net.State
             _adsSymbolInfo = AdsSymbolReader.Read(connection, _config.VariablePath);
         }
 
+        private void OnAdsNotificationError(object sender, AdsNotificationErrorEventArgs e)
+        {
+            _logger.Error(e.Exception, "ADS Notification error.");
+        }
+
         protected virtual void OnAdsNotification(object sender, AdsNotificationEventArgs e)
         {
             if (e.UserData != this)
                 return;
 
+            LogNotificationQueueOverflow();
+
             try
             {
                 TStatus status = _adsMapper.MapStream(e.DataStream);
-                CurrentSample = (DateTime.FromFileTime(e.TimeStamp), status);
+                var timestamp = DateTime.FromFileTime(e.TimeStamp);
 
-                OnStateChanged(status, DateTime.FromFileTime(e.TimeStamp));
+                CurrentSample = (timestamp, status);
+
+                _notificationExecutor.ExecuteAsync(() => OnStateChanged(status, timestamp));
             }
             catch (Exception ex)
             {
@@ -108,6 +136,22 @@ namespace Mbc.Pcs.Net.State
         protected virtual void OnStateChanged(TStatus status, DateTime dateTime)
         {
             StateChanged?.Invoke(this, new PlcStateChangedEventArgs<TStatus>(status, dateTime));
+        }
+
+        private void LogNotificationQueueOverflow()
+        {
+            _maxOverflow = Math.Max(_notificationExecutor.ExecutionQueueLength, _maxOverflow);
+            if (_notificationExecutor.ExecutionQueueLength > (_config.MaxDelay.TotalMilliseconds / _config.CycleTime.TotalMilliseconds * 2) && !_queueOverflowLogging)
+            {
+                _logger.Trace("ADS Notification queue started to overflow.");
+                _queueOverflowLogging = true;
+            }
+            else if (_notificationExecutor.ExecutionQueueLength == 0 && _queueOverflowLogging)
+            {
+                _logger.Warn("Ads Notification queue overflow max {maxOverflow}.", _maxOverflow);
+                _queueOverflowLogging = false;
+                _maxOverflow = 0;
+            }
         }
     }
 }
