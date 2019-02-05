2a01b2fdd8eadadc10d378c02c9eb929a4afffdc
Michael Helfenstein <michael.helfenstein@mbc-engineering.ch>
2019-02-05 11:18:10 +0100

    update copyright jeahr

--- NET/Mbc.Pcs.Net/IHeartBeat.cs
+++ NET/Mbc.Pcs.Net/IHeartBeat.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------------
-// Copyright (c) 2018 by mbc engineering GmbH, CH-6015 Luzern
+// Copyright (c) 2019 by mbc engineering GmbH, CH-6015 Luzern
 // Licensed under the Apache License, Version 2.0
 //-----------------------------------------------------------------------------
 
