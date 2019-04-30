0e320ce5a0961a5bdb057e39e878d26f2ef59dfa
Michael Helfenstein <michael.helfenstein@mbc-engineering.ch>
2019-04-30 08:29:51 +0200

    Add IPlcAlarmCommand interface

---
+++ NET/Mbc.Pcs.Net.Alarm/IPlcAlarmCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Mbc.Pcs.Net.Alarm
+{
+    public interface IPlcAlarmCommand
+    {
+        /// <summary>
+        /// Plc Kommando für "ResetAlarmsCommand".
+        /// </summary>
+        bool ResetAlarms();
+    }
+}
